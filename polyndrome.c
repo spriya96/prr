6111772dfc45abc83845540f5b9625e5f188361b
spriya96 <30333676+spriya96@users.noreply.github.com>
2017-08-12 17:01:27 +0530

    Create polyndrome.c

---
+++ polyndrome.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,reverse=0;remainder,orginal;
+printf("the number is");
+scanf("%d",&n);
+while(n!=0)
+{
+remainder=n%10;
+reverse=reverse+10;
+n=n/10
+}
+return 0;
+}
